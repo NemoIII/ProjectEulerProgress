34aadd0559ec68634ffcee0ed3fce1d9180aa117
ianderson.pablo <ianderson.pablo@betsete.com>
2020-02-10 10:56:30 +0000

    Ainda preciso ajustar o return Fibonacci e ajustar o memo[x]

--- TypeScript/problem2_1.js
+++ TypeScript/problem2_1.js
@@ -1,9 +1,10 @@
+// import {Memoize} from 'typescript-memoize'
 function Fibonacci(n) {
-    if (n <= 1) {
+    if (n === 0 || n === 1) {
         return n;
     }
     else {
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }
-console.log("Fibonacci", Fibonacci(10));
+console.log("Fibonacci", Fibonacci(9));
--- TypeScript/problem2_1.ts
+++ TypeScript/problem2_1.ts
@@ -1,8 +1,20 @@
-function Fibonacci(n:number) {
-    if (n<=1){
-        return n;
-    } else {
-        return Fibonacci(n-1) + Fibonacci(n-2);
+// import {Memoize} from 'typescript-memoize'
+
+
+function Fibonacci(n: number) {
+    let memo = {}
+
+    function f(x: number) {
+        let value;
+        if (x in memo) {
+            value = memo[x];
+        } else {
+            if (n === 0 || n === 1) {
+                return n;
+            } else {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
     }
 }
-console.log("Fibonacci", Fibonacci(10))
+console.log("Fibonacci", Fibonacci(9))
